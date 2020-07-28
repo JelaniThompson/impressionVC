35c71156614e6456dab0cc588a94c4efd658d0d6
Jelani Thompson <jelanithompson@Jelanis-MacBook-Pro.local>
2020-07-28 14:11:36 -0400

    Google Maps

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -140,6 +140,8 @@
           <img src="@/assets/images/ecosystem/500startups.jpg" alt="500 Startups Logo" class="networkImageItem">
         </div>
       </div>
+
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2884.157302335472!2d-79.4017364845009!3d43.70727995695!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b333cd1040c05%3A0xc42640fdd25816c1!2sImpression%20Ventures!5e0!3m2!1sen!2sca!4v1595959831225!5m2!1sen!2sca" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
   </div>
 </template>
 
